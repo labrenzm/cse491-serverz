ba177ebdf3c49694afbb946bace6d438443186bd
juru13 <rushjust@msu.edu>
2014-01-10 14:56:55 -0500

    Update with more efficient method

--- server.py
+++ server.py
@@ -13,12 +13,25 @@ print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
 s.listen(5)                 # Now wait for client connection.
 
+
+message = """
+
+HTTP/1.0 200 OK
+Content-Type: text/html
+
+<html>
+<h1>Hello, world</h1> this is rushjust's Web server.
+</html>
+
+
+"""
+
+
 print 'Entering infinite loop; hit CTRL-C to exit'
 while True:
-    # Establish connection with client.    
+    # Establish connection with client.
     c, (client_host, client_port) = s.accept()
     print c.recv(1000)
     print 'Got connection from', client_host, client_port
-    c.send('HTTP/1.0 200 OK\nDate: ' + time.asctime(time.gmtime()) + '\nContent-Type: text/html\n\n<html>\n<body>\n<h1>Hello, world</h1> this is rushjust web server\n</body>\n</html>\n')
+    c.send(message)
     c.close()
- 
