fe9d1cef80577c3ad4222a7f5ffa52489ba7f84a
Andrew Lieblich <lieblic2@arctic.cse.msu.edu>
2014-01-16 16:08:54 -0500

    review of hw1

--- server.py
+++ server.py
@@ -19,18 +19,23 @@ while True:
     c, (client_host, client_port) = s.accept()
     print c.recv(1000)
     print 'Got connection from', client_host, client_port
-    c.send('Thank you for connecting')
-    c.send("""\nHTTP/1.0 200 OK
+    # @comment add the \r\n for new lines
+    c.send('HTTP/1.0 200 OK \r\n')
+    c.send('Content-type:text/html \r\n\r\n')
+    c.send('<h1>hello world</h1>')
+    c.send('this is labrenzm\'s server')
+  #  c.send('Thank you for connecting')
+   # c.send("""\nHTTP/1.0 200 OK
  
-  Content-Type: text/html
+#  Content-Type: text/html
 
-  <html>
-  <head>
-  <body>
-  <h1>Hello, World!</h1>
-  this is labrenzm's web server!
-  </body>
-  </html>""")
+#  <html>
+#  <head>
+#  <body>
+#  <h1>Hello, World!</h1>
+#  this is labrenzm's web server!
+#  </body>
+#  </html>""")
   
-    c.send("\ngood bye.")
+#    c.send("\ngood bye.")
     c.close()
